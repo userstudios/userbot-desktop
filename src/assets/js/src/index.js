c5e4c0812e6bc33914b16418fecfdf6272e0f05b
IchBinHanz <nilssimons707@gmail.com>
2021-01-06 18:36:36 +0100

    v1.0

---
+++ src/assets/js/src/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const os = require('os');
+const http = require('http');
+const fileDialog = require('file-dialog')
+const path = require('path');
+const fetch = require('node-fetch');
+const jarfile = require("jarfile");
+
+const doc = document
