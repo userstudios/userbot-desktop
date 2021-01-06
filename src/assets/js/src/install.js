ebde1bfaf4dbb99720f76b22d19511114b9b09ac
Yato361 <63376584+Yato361@users.noreply.github.com>
2021-01-06 19:40:38 +0100

    Conventions

--- src/assets/js/src/install.js
+++ src/assets/js/src/install.js
@@ -11,33 +11,33 @@ var config_data = fs.readFileSync(path.join(__dirname, 'data/config.json'));
 var config_data = JSON.parse(config_data);
 
 try {
-  if (fs.existsSync(default_ub_path)) {
-    alert("Already Installed.")
-    inputInstallPath.value = "Already Installed."
-    checkUpdate()
-  } else {
-    secInstall.style.display = "block"
-    alert("Not Installed.")
-    inputInstallPath.value = default_mc_path
-    btnInstall.addEventListener('click', () => {
-        var install_path = default_mc_path + `\\LabyMod\\addons-1.12\\UserBot.jar`
-        console.log(install_path)
-        downloadLatest(install_path)
-        config_data.mc_path = default_mc_path
-        console.log(config_data)
-        fs.writeFileSync(path.join(__dirname, 'data/config.json'), JSON.stringify(config_data));
-    })
-  }
-} catch(err) {
+    if (fs.existsSync(default_ub_path)) {
+        alert("Already Installed.")
+        inputInstallPath.value = "Already Installed."
+        checkUpdate()
+    } else {
+        secInstall.style.display = "block"
+        alert("Not Installed.")
+        inputInstallPath.value = default_mc_path
+        btnInstall.addEventListener('click', () => {
+            var install_path = default_mc_path + `\\LabyMod\\addons-1.12\\UserBot.jar`
+            console.log(install_path)
+            downloadLatest(install_path)
+            config_data.mc_path = default_mc_path
+            console.log(config_data)
+            fs.writeFileSync(path.join(__dirname, 'data/config.json'), JSON.stringify(config_data));
+        })
+    }
+} catch (err) {
     console.log(err)
     alert("error")
 }
 
 async function downloadLatest(install_path) {
     const down_file = fs.createWriteStream(install_path);
-        http.get(config_data.download_server, function(response) {
-            response.pipe(down_file);
-            alert("Download finished!")
-            console.log('Download finished!')
-        });
+    http.get(config_data.download_server, function(response) {
+        response.pipe(down_file);
+        alert("Download finished!")
+        console.log('Download finished!')
+    });
 }
