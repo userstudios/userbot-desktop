0b067a5f21fe2d2390ca4ef27aafa303c5c0cd8d
Yato361 <63376584+Yato361@users.noreply.github.com>
2021-01-06 23:35:32 +0100

    Added other frontend

--- src/assets/js/src/install.js
+++ src/assets/js/src/install.js
@@ -1,37 +1,18 @@
 const default_mc_path = `C:\\Users\\${os.userInfo().username}\\AppData\\Roaming\\.minecraft`
 const default_ub_path = `C:\\Users\\${os.userInfo().username}\\AppData\\Roaming\\.minecraft\\LabyMod\\addons-1.12\\UserBot.jar`
-const secInstall = doc.querySelector('.sec-install')
-const inputInstallPath = doc.querySelector('.input-install-path')
-const btnInstall = doc.querySelector('.btn-install')
-const secUpdate = doc.querySelector('.sec-update')
-const btnUpdate = doc.querySelector('.btn-update')
-const secUptodate = doc.querySelector('.sec-uptodate')
+const inputInstallPath = document.querySelector('.input-install-path')
+const btnInstall = document.querySelector('.btn-install')
+const btnUpdate = document.querySelector('.btn-update')
+const secUptodate = document.querySelector('.sec-uptodate')
 
 var config_data = fs.readFileSync(path.join(__dirname, 'data/config.json'));
 var config_data = JSON.parse(config_data);
 
-try {
-    if (fs.existsSync(default_ub_path)) {
-        alert("Already Installed.")
-        inputInstallPath.value = "Already Installed."
-        checkUpdate()
-    } else {
-        secInstall.style.display = "block"
-        alert("Not Installed.")
-        inputInstallPath.value = default_mc_path
-        btnInstall.addEventListener('click', () => {
-            var install_path = default_mc_path + `\\LabyMod\\addons-1.12\\UserBot.jar`
-            console.log(install_path)
-            downloadLatest(install_path)
-            config_data.mc_path = default_mc_path
-            console.log(config_data)
-            fs.writeFileSync(path.join(__dirname, 'data/config.json'), JSON.stringify(config_data));
-        })
-    }
-} catch (err) {
-    console.log(err)
-    alert("error")
-}
+secUpdate.style.display = "block";
+
+btnInstall.addEventListener('click', () => {
+
+});
 
 async function downloadLatest(install_path) {
     const down_file = fs.createWriteStream(install_path);
