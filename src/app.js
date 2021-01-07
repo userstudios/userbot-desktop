401fd1fe0399b03101424f22581ea1bffd2fcf5f
0xtcb <63376584+0xtcb@users.noreply.github.com>
2021-01-07 01:12:17 +0100

    Added help to the installer

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const createWindow = () => {
     // Create the browser window.
     const win = new BrowserWindow({
         width: 1000,
-        height: 600,
+        height: 300,
         webPreferences: {
             nodeIntegration: true
         },
