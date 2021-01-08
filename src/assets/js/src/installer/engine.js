b052506ba9451e42a035bcdfdfad99cb39ea92d0
Yato361 <63376584+Yato361@users.noreply.github.com>
2021-01-08 06:30:50 +0100

    Fixxed promise bug

--- src/assets/js/src/installer/engine.js
+++ src/assets/js/src/installer/engine.js
@@ -1,17 +1,38 @@
-const default_mc_path = `C:\\Users\\${os.userInfo().username}\\AppData\\Roaming\\.minecraft`;
-const default_ub_path = `C:\\Users\\${os.userInfo().username}\\AppData\\Roaming\\.minecraft\\LabyMod\\addons-1.12\\UserBot.jar`;
 const inputInstallPath = document.getElementById('directorypath');
 const contractCheckbox = document.getElementById('contract');
 const installUpdateButton = document.getElementById('installUpdateButton');
-
 var config_data = JSON.parse(fs.readFileSync(path.join(__dirname, 'data/config.json')));
-
 
 // Register events
 window.onload = function() {
-    installUpdateButton.addEventListener('click', helloWorldHandler);
+    installUpdateButton.addEventListener('click', checkUserBotIsInstalled);
 };
 
-function helloWorldHandler() {
-    alert("Hello world");
+function checkUserBotIsInstalled() {
+    try {
+        if (!fs.existsSync(getConfigDownloadPath())) {
+            throw new Error("Path does not exist");
+        }
+        getAllJarFiles(getConfigDownloadPath()).then(files => {
+            console.log(files.length)
+        }).catch(err => {
+            alert("Verzeichnis konnte nicht geöffnet werden: " + err.message);
+        });
+    } catch (err) {
+        alert("Verzeichnis konnte nicht geöffnet werden: " + err.message);
+    }
+}
+
+function getAllJarFiles(path) {
+    return new Promise((resolve, reject) => {
+        glob(path + '/*.jar', {}, (err, files) => {
+            if (!err) resolve(files)
+            else reject(err)
+        })
+    });
+}
+
+function getConfigDownloadPath() {
+    let user = os.userInfo().username;
+    return config_data.download_path.replace("${USER}", user);
 }
