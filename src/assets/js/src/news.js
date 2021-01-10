a4a6390e24450ed32439e0566a15681ebfd6d8cb
Yato361 <63376584+Yato361@users.noreply.github.com>
2021-01-10 05:02:45 +0100

    Format & bugfix

--- src/assets/js/src/news.js
+++ src/assets/js/src/news.js
@@ -4,4 +4,4 @@ fetch(config_data.userbot_news)
     .then(res => res.text())
     .then(body => {
         news_letter.innerHTML = body;
-    }).catch(err => { newsLetter.innerHTML = config_data.userbot_no_news_connection });
+    }).catch(err => { newsLetter.innerHTML = config_data.userbot_no_news_connection + "<p>" + err + "</p>" });
