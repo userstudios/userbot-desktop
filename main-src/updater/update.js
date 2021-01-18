b683d35e849953e3ed02bad1b1e8680e6a60d768
Yato361 <63376584+Yato361@users.noreply.github.com>
2021-01-18 08:13:19 +0100

    Removed commented code

--- main-src/updater/update.js
+++ main-src/updater/update.js
@@ -26,33 +26,6 @@ async function update() {
     } else {
 
     }
-    // fetch('https://api.github.com/repos/Yato361/userbot-desktop/releases').then((res) => res.json().then((json) => {
-    //     if (json[0].tag_name != latestVersion) {
-    //         console.log(json[0].zipball_url)
-    //         fetch(json[0].assets[0].browser_download_url).then((response) => {
-    //             if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
-    //             streamPipeline(response.body, fs.createWriteStream(downloadPath + "\\" + foldername)).then(() => {
-    //                 if (fs.existsSync(path.join(__dirname, 'main-src'))) {
-    //                     trash(path.join(__dirname, 'main-src')).then(() => {
-    //                         extract(path.join(__dirname, foldername), { dir: path.join(__dirname) }).then(() => {
-    //                             trash(path.join(__dirname, foldername)).then(() => {
-    //                                 console.log("downloaded");
-    //                             });
-    //                         });
-    //                     });
-    //                 } else {
-    //                     extract(path.join(__dirname, foldername), { dir: path.join(__dirname) }).then(() => {
-    //                         trash(path.join(__dirname, foldername)).then(() => {
-    //                             console.log("downloaded");
-    //                         });
-    //                     });
-    //                 }
-    //             });
-    //         }).catch((err) => { console.log(err); });
-    //     } else {
-    //         console.log("already newest version")
-    //     }
-    // }));
 }
 
 module.exports.update = update;
