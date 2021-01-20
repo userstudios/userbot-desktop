4c7598bea39d92a971d2a88005c3917564744f86
Yato361 <63376584+Yato361@users.noreply.github.com>
2021-01-20 06:38:00 +0100

    Added createwindow to a function

--- main-src/assets/updater/js/index.js
+++ main-src/assets/updater/js/index.js
@@ -1,18 +1,21 @@
 const path = require('path');
 const { BrowserWindow } = require('electron').remote;
-let win = new BrowserWindow({
-    width: 500,
-    height: 800,
-    webPreferences: {
-        nodeIntegration: true,
-        enableRemoteModule: true,
-        contextIsolation: false
-    },
-    icon: path.join(__dirname, '../../../data/app.ico'),
-    resizable: false,
-    autoHideMenuBar: true
-});
 
-win.loadFile(path.join(__dirname, '../../../assets/html/src/index.html'));
-win.webContents.openDevTools();
-window.close();
+function createMainAppAndCloseUpdater() {
+    let win = new BrowserWindow({
+        width: 500,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            contextIsolation: false
+        },
+        icon: path.join(__dirname, '../../../data/app.ico'),
+        resizable: false,
+        autoHideMenuBar: true
+    });
+
+    win.loadFile(path.join(__dirname, '../../../assets/html/src/index.html'));
+    win.webContents.openDevTools();
+    window.close();
+}
