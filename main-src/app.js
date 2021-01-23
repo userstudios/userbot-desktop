386f28dc0bdbf919bd9a78d5e1827b429d7a71c7
0xtcb <63376584+0xtcb@users.noreply.github.com>
2021-01-23 14:40:27 +0100

    Added updater & updated package.json

--- main-src/app.js
+++ main-src/app.js
@@ -1,12 +1,14 @@
 const request = require('request');
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const { autoUpdater } = require('electron-updater');
+const { create } = require('electron-log');
 
 if (require('electron-squirrel-startup')) {
     app.quit();
 }
 
-const createWindow = async() => {
+async function checkForUpdateAndCreateWindow() {
     let updater = new BrowserWindow({
         width: 200,
         height: 200,
@@ -21,9 +23,28 @@ const createWindow = async() => {
         autoHideMenuBar: true
     });
     updater.loadFile(path.join(__dirname, 'assets/updater/html/index.html'));
+    autoUpdater.checkForUpdatesAndNotify();
+}
+
+async function createWindow() {
+    let win = new BrowserWindow({
+        width: 500,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            contextIsolation: false
+        },
+        icon: path.join(__dirname, '../../../data/app.ico'),
+        resizable: false,
+        autoHideMenuBar: true
+    });
+
+    win.loadFile(path.join(__dirname, '../../../assets/html/src/index.html'));
+    win.webContents.openDevTools();
 };
 
-app.on('ready', createWindow);
+app.on('ready', checkForUpdateAndCreateWindow);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
@@ -35,4 +56,23 @@ app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
+});
+
+autoUpdater.on('checking-for-update', () => {
+    //TODO: notify user
+})
+autoUpdater.on('update-available', (info) => {
+    //TODO: notify user
+})
+autoUpdater.on('update-not-available', (info) => {
+    //TODO: close updater and start main program
+})
+autoUpdater.on('error', (err) => {
+    //TODO: notify user, emit event to js file linked with the updater and retry in a couple of secounds
+})
+autoUpdater.on('download-progress', (progressObj) => {
+    //TODO: print progress on html
+})
+autoUpdater.on('update-downloaded', (info) => {
+    autoUpdater.quitAndInstall();
 });
--- main-src/assets/updater/js/index.js
+++ main-src/assets/updater/js/index.js
@@ -2,22 +2,3 @@ const path = require('path');
 const { BrowserWindow } = require('electron').remote;
 
 alert('App starting...');
-
-function createMainAppAndCloseUpdater() {
-    let win = new BrowserWindow({
-        width: 500,
-        height: 800,
-        webPreferences: {
-            nodeIntegration: true,
-            enableRemoteModule: true,
-            contextIsolation: false
-        },
-        icon: path.join(__dirname, '../../../data/app.ico'),
-        resizable: false,
-        autoHideMenuBar: true
-    });
-
-    win.loadFile(path.join(__dirname, '../../../assets/html/src/index.html'));
-    win.webContents.openDevTools();
-    window.close();
-}
