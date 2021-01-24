0bed6a27bbda6716bc6de75a143d8a128dd4eeab
Yato361 <63376584+Yato361@users.noreply.github.com>
2021-01-24 16:17:52 +0100

    Removed loging

--- main-src/assets/updater/js/index.js
+++ main-src/assets/updater/js/index.js
@@ -4,18 +4,15 @@ const ipcRenderer = require('electron').ipcRenderer;
 var updateStatus = document.getElementById('update_status');
 
 ipcRenderer.on('loading_bar', (event, arg) => {
-    log(arg);
     changePercentileOfBar(arg)
 });
 
 ipcRenderer.on('loading_status', (event, arg) => {
-    log(arg);
     updateStatus.innerHTML = arg;
 });
 
 function changePercentileOfBar(percent) {
-    document.documentElement.style.setProperty('--percentile', percent + "%")
-    log("added " + percent)
+    document.documentElement.style.setProperty('--percentile', percent + "%");
 }
 
 function log(msg) {
