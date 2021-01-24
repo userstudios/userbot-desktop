72f7974fc96a5b645f6d33fb5c4ef86e46aeafac
Yato361 <63376584+Yato361@users.noreply.github.com>
2021-01-24 12:44:09 +0100

    Added ipc events to the updater

--- main-src/assets/updater/js/index.js
+++ main-src/assets/updater/js/index.js
@@ -1,2 +1,23 @@
 const path = require('path');
 const { BrowserWindow } = require('electron').remote;
+const ipcRenderer = require('electron').ipcRenderer;
+var updateStatus = document.getElementById('update_status');
+var loader = document.getElementsByClassName('loader');
+var loading_1 = document.getElementsByClassName('loading_1');
+console.log(loader.style)
+ipcRenderer.on('loading_bar', (event, arg) => {
+    changePercentileOfBar(arg[0])
+});
+
+ipcRenderer.on('loading_status', (event, arg) => {
+    updateStatus.innerHTML = arg[0];
+});
+
+function changePercentileOfBar(percent) {
+    for (let i = 0; i < loader.length; i++) {
+
+    }
+    for (let i = 0; i < loading_1.length; i++) {
+
+    }
+}
