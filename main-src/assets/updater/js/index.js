83f3a5696cfb39ec4348dc1b00f4d74c70ffe8e2
Yato361 <63376584+Yato361@users.noreply.github.com>
2021-01-24 13:33:21 +0100

    Added loading bar with percentile

--- main-src/assets/updater/js/index.js
+++ main-src/assets/updater/js/index.js
@@ -2,8 +2,6 @@ const path = require('path');
 const { BrowserWindow } = require('electron').remote;
 const ipcRenderer = require('electron').ipcRenderer;
 var updateStatus = document.getElementById('update_status');
-var loader = document.getElementsByClassName('loader');
-var loading_1 = document.getElementsByClassName('loading_1');
 
 ipcRenderer.on('loading_bar', (event, arg) => {
     log(arg);
@@ -16,12 +14,7 @@ ipcRenderer.on('loading_status', (event, arg) => {
 });
 
 function changePercentileOfBar(percent) {
-    for (let i = 0; i < loader.length; i++) {
-
-    }
-    for (let i = 0; i < loading_1.length; i++) {
-
-    }
+    document.documentElement.style.setProperty('--percentile', percent)
 }
 
 function log(msg) {
